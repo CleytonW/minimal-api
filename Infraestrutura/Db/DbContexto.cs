04ca9bebccb181d8721f1a17cec028bf19153b16
Cleyton Melo <cleytonw11@gmail.com>
2025-09-21 12:30:20 -0300

    Adicionando banco de dados

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MinimalApi.Domain.Entities;
+
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
-    
+    private readonly IConfiguration _configurationAppSettings;
+    public DbContexto(IConfiguration configurationAppSettings)
+    {
+        _configurationAppSettings = configurationAppSettings;
+    }
+
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var stringConnection = _configurationAppSettings.GetConnectionString("MySql")?.ToString();
+            if (!string.IsNullOrEmpty(stringConnection))
+            {
+                optionsBuilder.UseMySql(
+                    stringConnection, ServerVersion.AutoDetect(stringConnection)
+                );
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
-// using MinimalApi.Domain.DTOs;
+using MinimalApi.Domain.DTOs;
+using MinimalApi.Infraestrutura.Db;
+
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MySql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySql"))
+    );
+});
+
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.Domain.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", (LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
     {
